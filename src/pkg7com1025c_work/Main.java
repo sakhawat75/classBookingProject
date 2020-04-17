1e5b995e2929c50b4de653449ed82d18807514d0
sakhawat <sakhawat@sakhawats-mbp.lan>
2020-04-17 12:48:44 +0100

    Testing project after adding to bitbucket repository.

--- src/pkg7com1025c_work/Main.java
+++ src/pkg7com1025c_work/Main.java
@@ -11,11 +11,11 @@ package pkg7com1025c_work;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hello World");
+        
+        
     }
     
 }
